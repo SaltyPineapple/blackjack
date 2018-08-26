7779b173c570e46cbae1fafa12fa6f1ee72d16bc
SaltyPineapple <mjpratt31@gmail.com>
2018-08-25 20:32:42 -0700

    Correctly Prints Facecards

--- Game.py
+++ Game.py
@@ -2,10 +2,11 @@ import random
 import math
 from time import sleep
 
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10,
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10,
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10,
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 
 playerHand = []
 dealerHand = []
@@ -13,12 +14,27 @@ dealerHand = []
 
 def dealPrint(string):
     for letter in string:
+        if letter == 1:
+            letter = "A"
+        if letter == 11:
+            letter = "J"
+        if letter == 12:
+            letter = "Q"
+        if letter == 13:
+            letter = "K"
         print(letter, end=" ")
-        sleep(.5)
+        sleep(.6)
+
+
+def Sum(inside):
+    for x in inside:
+        if x > 10:
+            x = 10
+    return sum(inside)
 
 
 def dealer():
-    while sum(dealerHand) < 17:
+    while Sum(dealerHand) < 17:
         dealerHand.append(deck.pop())
 
 
@@ -26,8 +42,9 @@ def choice():
     choose = input("\nHit? [Y / N]")
     if choose == "y":
         playerHand.append(deck.pop())
+        print("\nPlayer:", end="")
         dealPrint(playerHand)
-        if sum(playerHand) < 21:
+        if Sum(playerHand) < 21:
             choice()
     else:
         return "n"
@@ -39,7 +56,11 @@ def hand():
     dealerHand.append(deck.pop())
     playerHand.append(deck.pop())
     dealerHand.append(deck.pop())
+    print("\nPlayer:", end="")
     dealPrint(playerHand)
+    print("\nDealer:", end="")
+    dealPrint(dealerHand)
+
     if choice() == "n":
         return
 
@@ -52,33 +73,35 @@ def game():
         wager = int(input("You have $" + str(cash) + ". How much would you like to wager?"))
         hand()
         dealer()
-        print("\nDealer:" + str(dealerHand))
-        print("\nPlayer:" + str(playerHand))
-        if (sum(playerHand) == 21) & (sum(dealerHand) != 21):
+        print("\nDealer:", end="")
+        dealPrint(dealerHand)
+        print("\nPlayer:", end="")
+        dealPrint(playerHand)
+        if (Sum(playerHand) == 21) & (Sum(dealerHand) != 21):
             print("\nBLACKJACK!")
             print("You got $" + str(wager))
             cash = wager + cash
-        elif (sum(dealerHand) == 21) & (sum(playerHand) != 21):
+        elif (Sum(dealerHand) == 21) & (Sum(playerHand) != 21):
             print("\nDEALER BLACKJACK!")
             print("You lost $" + str(wager))
             cash = cash - wager
-        elif sum(playerHand) > 21:
+        elif Sum(playerHand) > 21:
             print("\nBUST!")
             print("You lost $" + str(wager))
             cash = cash - wager
-        elif (sum(dealerHand) > 21) & (sum(playerHand) < 21):
+        elif (Sum(dealerHand) > 21) & (Sum(playerHand) < 21):
             print("\nDEALER BUST!")
             print("You got $" + str(wager))
             cash = cash + wager
-        elif (sum(playerHand) > sum(dealerHand)) & (sum(playerHand) < 21):
+        elif (Sum(playerHand) > Sum(dealerHand)) & (Sum(playerHand) < 21):
             print("\nYOU WON!")
             print("You got $" + str(wager))
             cash = cash + wager
-        elif (sum(playerHand) < sum(dealerHand)) & (sum(dealerHand) < 21):
+        elif (Sum(playerHand) < Sum(dealerHand)) & (Sum(dealerHand) < 21):
             print("\nYOU LOST!")
             print("You lost $" + str(wager))
             cash = cash - wager
-        elif (sum(playerHand) == sum(dealerHand)):
+        elif Sum(playerHand) == Sum(dealerHand):
             print("\nTIE! You get your wager back")
             cash = cash
         again = input("want to play another hand? [y / n]")
@@ -87,6 +110,7 @@ def game():
         else:
             playerHand.clear()
             dealerHand.clear()
+
     print("Thanks for playing! You ended with $" + str(cash))
 
 
